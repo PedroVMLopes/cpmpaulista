455cd1ea98c29a95e77e25930f635ee69a735f3e
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-06-25 17:51:01 -0300

    Box Changes

--- components/AssessoriaFinanceira.tsx
+++ components/AssessoriaFinanceira.tsx
@@ -4,39 +4,37 @@ export default function AssessoriaFinanceira() {
     return (
         <div className="text-center flex flex-col items-center">
             <h1 className="text-3xl py-4">Assessoria Financeira</h1>
-            <p className="p-2 md:max-w-[60%]">
-                Que tal dedicar mais tempo para seus clientes?
-            </p>
-            <p className="p-2 md:max-w-[50%]">
-                Libere sua equipe para fazer o que realmente importa.
-                Nossa metodologia de trabalho automatiza tarefas e reduz a rotina financeira de sua empresa.
-            </p>
-            <div className="grid grid-cols-1 md:grid-cols-3 w-full p-2 gap-8 md:gap-2 mt-18">
+            <div className="grid grid-cols-1 md:grid-rows-2 md:grid-cols-2 w-full p-2 gap-1 mt-10">
                 {/* Grid 1 */}
-                <div className="indicator w-full flex flex-col justify-around rounded-2xl bg-neutral text-primary-content">
-                    <FaCamera className="indicator-item indicator-center bg-neutral w-10 h-8 p-1.5 rounded-2xl"/>
-                    <p className="text-xl p-4 pt-6 pb-0 text-neutral-content">Envie fotos das suas contas</p>
-                    <div className="p-4 flex flex-col gap-2">
-                        <p>Nós cuidamos de todo o resto.</p>
-                        <p>Identificamos, Categorizamos e Agendamos suas Contas a Pagar e a Receber</p>
+                <div className="indicator w-full flex flex-col justify-around rounded-2xl rounded-b-none bg-primary text-primary-content">
+                    {/* <FaCamera className="indicator-item indicator-center bg-neutral w-10 h-8 p-1.5 rounded-2xl"/> */}
+                    <p className="text-2xl p-4 pt-6 pb-0 text-neutral-content">Gestão Financeira (BPO)</p>
+                    <div className="p-4 pb-6 flex flex-col gap-2">
+                        <p>Controle completo das finanças da empresa para apoiar decisões seguras e estratégicas. Acompanhamos o fluxo de caixa, realizamos a conciliação bancária e garantimos informações confiáveis que contribuem para a saúde financeira e o crescimento do negócio.</p>
                     </div>
                 </div>
                 {/* Grid 2 */}
-                <div className="indicator w-full flex flex-col justify-around rounded-2xl bg-neutral text-primary-content">
-                    <FaCheckDouble className="indicator-item indicator-center bg-neutral text-white w-10 h-8 p-1.5 rounded-2xl"/>
-                    <p className="text-xl p-4 pt-6 pb-0 text-neutral-content">Aprove os Pagamentos</p>
-                    <div className="p-4 flex flex-col gap-2">
-                        <p>Aprove o pagamento de suas contas no seu Internet Banking.</p>
-                        <p>Nossa equipe faz a Reconciliação Bancária para manter suas informações financeiras sempre atualizadas</p>
+                <div className="indicator w-full flex flex-col justify-around bg-primary text-primary-content">
+                    {/* <FaCheckDouble className="indicator-item indicator-center bg-neutral text-white w-10 h-8 p-1.5 rounded-2xl"/> */}
+                    <p className="text-2xl p-4 pt-6 pb-0 text-neutral-content">Gestão de Indicadores de Desempenho (KPIs)</p>
+                    <div className="p-4 pb-6 flex flex-col gap-2">
+                        <p>Acompanhamento dos principais indicadores financeiros e operacionais do negócio. Analisamos a performance da empresa, identificamos tendências, pontos de melhoria e oportunidades de crescimento.</p>
                     </div>
                 </div>
                 {/* Grid 3 */}
-                <div className="indicator w-full flex flex-col justify-around rounded-2xl bg-neutral text-primary-content">
-                    <FaNewspaper className="indicator-item indicator-center bg-neutral text-white w-10 h-8 p-1.5 rounded-2xl"/>
-                    <p className="text-xl p-4 pt-6 pb-0 text-neutral-content">Relatórios em tempo real</p>
-                    <div className="p-4 flex flex-col gap-2">
-                        <p>Tenha um robusto dashboard com toda a situação financeira de sua empresa.</p>
-                        <p>Saiba exatamente de onde veio e para onde foi o dinheiro. Tenha em mãos o Fluxo de Caixa atualizado com suas Contas a Pagar e a Receber</p>
+                <div className="indicator w-full flex flex-col justify-around bg-primary text-primary-content">
+                    {/* <FaNewspaper className="indicator-item indicator-center bg-neutral text-white w-10 h-8 p-1.5 rounded-2xl"/> */}
+                    <p className="text-2xl p-4 pt-6 pb-0 text-neutral-content">Análise de Viabilidade Econômica</p>
+                    <div className="p-4 pb-6 flex flex-col gap-2">
+                        <p>Estudo detalhado sobre a viabilidade financeira de projetos ou investimentos, considerando custos, riscos e o retorno esperado, para garantir segurança nas decisões estratégicas.</p>
+                    </div>
+                </div>
+                {/* Grid 4 */}
+                <div className="indicator w-full flex flex-col justify-around rounded-2xl rounded-t-none bg-primary text-primary-content">
+                    {/* <FaNewspaper className="indicator-item indicator-center bg-neutral text-white w-10 h-8 p-1.5 rounded-2xl"/> */}
+                    <p className="text-2xl p-4 pt-6 pb-0 text-neutral-content">Business Valuation</p>
+                    <div className="p-4 pb-6 flex flex-col gap-2">
+                        <p>Avaliação econômico-financeira da empresa ou de projetos, com estimativa de valor baseada em modelos técnicos e na percepção de mercado.</p>
                     </div>
                 </div>
             </div>
