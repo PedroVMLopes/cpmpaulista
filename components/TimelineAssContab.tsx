c7d1fe1c5f43926c79ee1f2423a54a009a5a30e7
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-06-25 15:34:51 -0300

    New timeline texts

--- components/TimelineAssContab.tsx
+++ components/TimelineAssContab.tsx
@@ -17,7 +17,7 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-start timeline-box flex flex-col p-2 py-2.5"><p className="text-base font-semibold text-neutral">CONTABILIDADE</p><p className="text-sm">Assessoria contábil para empresas de pequenos e médio porte</p></div>
+                    <div className="timeline-start timeline-box flex flex-col p-2 py-2.5"><p className="text-base font-semibold text-neutral">Gestão Contábil</p><p className="text-sm">Elaboração das demonstrações contábeis para acompanhar o desempenho do negócio, seguindo as normas contábeis e requisitos legais</p></div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -36,7 +36,7 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-end timeline-box"><p className="text-base font-semibold text-neutral">TRIBUTÁRIA</p><p className="text-sm">Apuração de impostos municipais, estaduais e federais</p></div>
+                    <div className="timeline-end timeline-box"><p className="text-base font-semibold text-neutral">Gestão Tributária</p><p className="text-sm">Planejamento e controle da carga tributária para garantir o cumprimento das obrigações fiscais em todas as esferas (Federal, Estadual e Municipal), abrangendo impostos, taxas e contribuições</p></div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -55,7 +55,7 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-start timeline-box"><p className="text-base font-semibold text-neutral">TRABALHISTA</p><p className="text-sm">Coordenação de Recursos Humanos e Folha de Pagamento</p></div>
+                    <div className="timeline-start timeline-box"><p className="text-base font-semibold text-neutral">Gestão Trabalhista</p><p className="text-sm">Controle da folha de pagamento e cumprimento das obrigações trabalhistas de acordo com a legislação e o e-Social</p></div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -74,7 +74,26 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-end timeline-box"><p className="text-base font-semibold text-neutral">SOCIETÁRIO</p><p className="text-sm">Abertura e encerramento de empresas e Alterações contratuais</p></div>
+                    <div className="timeline-end timeline-box"><p className="text-base font-semibold text-neutral">Controle Patrimonial</p><p className="text-sm">Gerenciamento de todos os bens da empresa, incluindo ativos tangíveis (equipamentos, imóveis) e intangíveis (marcas, patentes, entre outros)</p></div>
+                    <hr className="bg-primary"/>
+                </li>
+                <li>
+                    <hr className="bg-primary"/>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5"
+                        >
+                            <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                    <div className="timeline-start timeline-box"><p className="text-base font-semibold text-neutral">Controle de Custos</p><p className="text-sm">Identificação e análise dos custos das atividades da empresa, ajudando a reduzir gastos e aumentar a lucratividade</p></div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -93,7 +112,7 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-start timeline-box"><p className="text-base font-semibold text-neutral">REGIME TRIBUTÁRIO</p><p className="text-sm">Simples Nacional, Lucro Presumido e Lucro Real</p></div>
+                    <div className="timeline-end timeline-box"><p className="text-base font-semibold text-neutral">Business Intelligence (BI)</p><p className="text-sm">Coleta, organização e análise de dados do negócio, transformando-os em informações estratégicas para apoiar as decisões da empresa</p></div>
                     <hr className="bg-base-100"/>
                 </li>
             </ul>
