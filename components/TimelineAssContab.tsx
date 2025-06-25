068f32667b1ae2f44f5a2bc5ddde452f9addc60c
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-06-25 16:11:48 -0300

    Timeline refactor

--- components/TimelineAssContab.tsx
+++ components/TimelineAssContab.tsx
@@ -1,7 +1,7 @@
 export default function Timeline() {
     return (
         <div className="md:max-w-[80%] flex items-center justify-center">
-            <ul className="timeline timeline-vertical">
+            <ul className="timeline timeline-snap-icon timeline-vertical max-md:timeline-compact">
                 <li>
                     <div className="timeline-middle">
                         <svg
@@ -17,7 +17,10 @@ export default function Timeline() {
                             />
                         </svg>
                     </div>
-                    <div className="timeline-start timeline-box shadow-md py-4 flex flex-col p-2"><p className="text-base font-semibold text-neutral">Gestão Contábil</p><p className="text-sm">Elaboração das demonstrações contábeis para acompanhar o desempenho do negócio, seguindo as normas contábeis e requisitos legais</p></div>
+                    <div className="timeline-start mb-12 flex flex-col px-2 md:text-end">
+                        <p className="text-base font-semibold text-neutral">Gestão Contábil</p>
+                        <p className="text-sm">Elaboração das demonstrações contábeis para acompanhar o desempenho do negócio, seguindo as normas contábeis e requisitos legais</p>
+                    </div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -36,7 +39,10 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-end timeline-box shadow-md py-4"><p className="text-base font-semibold text-neutral">Gestão Tributária</p><p className="text-sm">Planejamento e controle da carga tributária para garantir o cumprimento das obrigações fiscais em todas as esferas (Federal, Estadual e Municipal), abrangendo impostos, taxas e contribuições</p></div>
+                    <div className="timeline-end mb-12 md:text-start md:px-2">
+                        <p className="text-base font-semibold text-neutral">Gestão Tributária</p>
+                        <p className="text-sm">Planejamento e controle da carga tributária para garantir o cumprimento das obrigações fiscais em todas as esferas (Federal, Estadual e Municipal), abrangendo impostos, taxas e contribuições</p>
+                    </div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -55,7 +61,10 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-start timeline-box shadow-md py-4"><p className="text-base font-semibold text-neutral">Gestão Trabalhista</p><p className="text-sm">Controle da folha de pagamento e cumprimento das obrigações trabalhistas de acordo com a legislação e o e-Social</p></div>
+                    <div className="timeline-start mb-12 md:text-end md:px-2">
+                        <p className="text-base font-semibold text-neutral">Gestão Trabalhista</p>
+                        <p className="text-sm">Controle da folha de pagamento e cumprimento das obrigações trabalhistas de acordo com a legislação e o e-Social</p>
+                    </div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -74,7 +83,10 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-end timeline-box shadow-md py-4"><p className="text-base font-semibold text-neutral">Controle Patrimonial</p><p className="text-sm">Gerenciamento de todos os bens da empresa, incluindo ativos tangíveis (equipamentos, imóveis) e intangíveis (marcas, patentes, entre outros)</p></div>
+                    <div className="timeline-end mb-12 md:text-start md:px-2">
+                        <p className="text-base font-semibold text-neutral">Controle Patrimonial</p>
+                        <p className="text-sm">Gerenciamento de todos os bens da empresa, incluindo ativos tangíveis (equipamentos, imóveis) e intangíveis (marcas, patentes, entre outros)</p>
+                    </div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -93,7 +105,10 @@ export default function Timeline() {
                             />
                         </svg>
                     </div>
-                    <div className="timeline-start timeline-box shadow-md py-4"><p className="text-base font-semibold text-neutral">Controle de Custos</p><p className="text-sm">Identificação e análise dos custos das atividades da empresa, ajudando a reduzir gastos e aumentar a lucratividade</p></div>
+                    <div className="timeline-start mb-12 md:text-end md:px-2">
+                        <p className="text-base font-semibold text-neutral">Controle de Custos</p>
+                        <p className="text-sm">Identificação e análise dos custos das atividades da empresa, ajudando a reduzir gastos e aumentar a lucratividade</p>
+                    </div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
@@ -112,7 +127,10 @@ export default function Timeline() {
                         />
                     </svg>
                     </div>
-                    <div className="timeline-end timeline-box shadow-md py-4"><p className="text-base font-semibold text-neutral">Business Intelligence (BI)</p><p className="text-sm">Coleta, organização e análise de dados do negócio, transformando-os em informações estratégicas para apoiar as decisões da empresa</p></div>
+                    <div className="timeline-end mb-12 md:text-start md:px-2">
+                        <p className="text-base font-semibold text-neutral">Business Intelligence (BI)</p>
+                        <p className="text-sm">Coleta, organização e análise de dados do negócio, transformando-os em informações estratégicas para apoiar as decisões da empresa</p>
+                    </div>
                     <hr className="bg-base-100"/>
                 </li>
             </ul>
