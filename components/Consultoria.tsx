b77f5afe41c12c941821db7a29a53625ea0b45b7
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-07-15 19:09:17 -0300

    feat: Footer numbers added

--- components/Consultoria.tsx
+++ components/Consultoria.tsx
@@ -1,21 +1,21 @@
 export default function Consultoria() {
     return (
         <div className="text-center flex flex-col items-center mt-24">
-            <h1 className="text-3xl md:text-4xl mb-12">Consultoria Empresarial</h1>
-            <div className="flex flex-col w-full text-start border-2 gap-0.5 rounded-2xl shadow-xl">
-                <div className="collapse collapse-arrow bg-base-100 rounded-b">
+            <h1 className="text-3xl md:text-4xl mb-10">Consultoria Empresarial</h1>
+            <div className="flex flex-col w-full text-start rounded-2xl gap-1">
+                <div className="collapse collapse-arrow bg-secondary text-base-300 rounded-b p-2">
                     <input type="radio" name="accordion-ServicosParaEmpresas" defaultChecked />
-                    <div className="collapse-title font-semibold text-xl">Estruturação Societária</div>
+                    <div className="collapse-title font-semibold text-xl text-base-100">Estruturação Societária</div>
                     <div className="collapse-content">Acompanhamento completo no processo de abertura e legalização da empresa, com suporte em todas as etapas junto à Junta Comercial, Receita Federal e demais órgãos competentes.</div>
                 </div>
-                <div className="collapse collapse-arrow bg-base-100 rounded">
+                <div className="collapse collapse-arrow bg-secondary text-base-300 rounded p-2">
                     <input type="radio" name="accordion-ServicosParaEmpresas" />
-                    <div className="collapse-title font-semibold text-xl">Planejamento Tributário</div>
+                    <div className="collapse-title font-semibold text-xl text-base-100">Planejamento Tributário</div>
                     <div className="collapse-content">Orientação estratégica para escolher o regime tributário mais vantajoso e garantir o cumprimento das obrigações legais. Aproveitamos os benefícios fiscais disponíveis e acompanhamos as mudanças da legislação, incluindo a Reforma Tributária.</div>
                 </div>
-                <div className="collapse collapse-arrow bg-base-100 rounded-t">
+                <div className="collapse collapse-arrow bg-secondary text-base-300 rounded-t p-2">
                     <input type="radio" name="accordion-ServicosParaEmpresas" />
-                    <div className="collapse-title font-semibold text-xl">Credenciamentos e Registros</div>
+                    <div className="collapse-title font-semibold text-xl text-base-100">Credenciamentos e Registros</div>
                     <div className="collapse-content">Auxiliamos no credenciamento, cadastros e registros necessários junto a instituições públicas e privadas, garantindo que sua empresa esteja apta a atender exigências legais ou participar de licitações, processos seletivos e projetos.</div>
                 </div>
             </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,37 @@
+import { FaGraduationCap, FaFlagCheckered } from "react-icons/fa6";
+import { BsBuildingFillAdd } from "react-icons/bs";
+
 export default function Footer() {
     return (
-        <div className="mt-36">
-            Footer
+        <div className="mt-36 w-full flex flex-col items-center pb-10">
+            <div className="stats stats-vertical md:stats-horizontal">
+                <div className="stat">
+                    {/* <div className="stat-figure text-secondary text-3xl">
+                        <FaGraduationCap />
+                    </div> */}
+                    <div className="stat-title">Mais de</div>
+                    <div className="stat-value">20 ANOS</div>
+                    <div className="stat-desc">de Experiência</div>
+                </div>
+
+                <div className="stat">
+                    {/* <div className="stat-figure text-secondary text-3xl">
+                        <BsBuildingFillAdd />
+                    </div> */}
+                    <div className="stat-title">Mais de </div>
+                    <div className="stat-value">120</div>
+                    <div className="stat-desc">Empresas Atendidas</div>
+                </div>
+
+                <div className="stat">
+                    {/* <div className="stat-figure text-secondary text-3xl">
+                        <FaFlagCheckered />
+                    </div> */}
+                    <div className="stat-title">Mais de</div>
+                    <div className="stat-value">2000</div>
+                    <div className="stat-desc">Declarações de IRPF entregues</div>
+                </div>
+            </div>
         </div>
     )
 }
